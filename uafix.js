9c8e27d0f3f136cd759d545d1f32b3d06cfbac15
edarchenko <edarchenko@gmail.com>
2026-03-15 22:59:44 +0200

    Update uafix.js

--- uafix.js
+++ uafix.js
@@ -1,19 +1,18 @@
 (function () {
     'use strict';
 
-    var pluginVersion = '1.0.8'; 
+    var pluginVersion = '1.0.9'; 
     var DOMAIN = 'https://uafix.net';
 
-    function startPlugin() {
-        Lampa.Noty.show('✅ UAFix v' + pluginVersion + ' успішно запущено!');
-    }
+    // Одразу при старті файлу показуємо сповіщення (без затримок)
+    Lampa.Noty.show('✅ UAFix v' + pluginVersion + ' завантажено!');
 
-    // Слухаємо відкриття сторінки фільму
+    // Перехоплюємо побудову сторінки фільму (як це робить online_mod)
     Lampa.Listener.follow('full', function (e) {
         if (e.type == 'build') {
             
             // Створюємо кнопку
-            var btn = $('<div class="full-start__button selector" style="background: #e50914;"><svg viewBox="0 0 24 24"><path fill="currentColor" d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M10,16.5V7.5L16,12L10,16.5Z" /></svg><span>UAFix Play</span></div>');
+            var btn = $('<div class="full-start__button selector button--icon" data-type="uafix" style="background: #e50914; margin-right: 10px; z-index: 9999;"><svg viewBox="0 0 24 24"><path fill="currentColor" d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M10,16.5V7.5L16,12L10,16.5Z" /></svg><span>UAFix Play</span></div>');
             
             // Дія при натисканні
             btn.on('hover:enter click', function () {
@@ -30,19 +29,21 @@
                 });
             });
 
-            // Чекаємо півсекунди, поки Лампа намалює інтерфейс, і вставляємо кнопку
-            setTimeout(function() {
-                var wrap = e.html.find('.full-start__buttons, .view--buttons, .info__buttons').first();
-                if (wrap.length) {
-                    wrap.append(btn);
+            // Шукаємо ВСІ можливі варіанти блоків кнопок у Лампі
+            var wrap = e.html.find('.full-start__buttons, .info__buttons, .view--buttons, .tv-buttons, .movies-buttons, .card__buttons').first();
+            
+            if (wrap.length) {
+                // Якщо знайшли - додаємо туди
+                wrap.append(btn);
+            } else {
+                // ЕКСТРЕНИЙ ВАРІАНТ: якщо немає жодного блоку кнопок, ліпимо кнопку прямо під постером або заголовком
+                var fallbackWrap = e.html.find('.full-start__info, .view--info, .tv-info').first();
+                if (fallbackWrap.length) {
+                    fallbackWrap.append(btn);
                 } else {
-                    Lampa.Noty.show('Помилка: не знайшов куди вставити кнопку');
+                    e.html.prepend(btn); // Просто зверху на сторінку
                 }
-            }, 500);
+            }
         }
     });
-
-    if (window.appready) startPlugin();
-    else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') startPlugin(); });
-
 })();
