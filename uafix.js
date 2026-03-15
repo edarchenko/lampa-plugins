c2c64e0f24adf61815f996c29ea3506f419249b3
edarchenko <edarchenko@gmail.com>
2026-03-15 23:05:44 +0200

    Update uafix.js

--- uafix.js
+++ uafix.js
@@ -1,39 +1,39 @@
 (function () {
     'use strict';
 
-    function initButton() {
-        // Слухаємо подію побудови сторінки фільму
-        Lampa.Listener.follow('full', function (e) {
-            if (e.type == 'build') {
-                
-                // Шукаємо стандартні блоки кнопок Лампи
-                var wrap = e.html.find('.full-start__buttons, .info__buttons, .view--buttons').first();
-                
-                if (wrap.length) {
-                    // Створюємо найпростішу кнопку без складних іконок
-                    var btn = $('<div class="full-start__button selector" style="background: #e50914;"><span>🔴 UAFix Test</span></div>');
-                    
-                    // Реакція на клік
-                    btn.on('hover:enter click', function () {
-                        Lampa.Noty.show('Ура! Кнопка працює!');
-                    });
+    var pluginVersion = '1.1.1';
 
-                    // Вставляємо кнопку
-                    wrap.append(btn);
-                }
+    function startPlugin() {
+        Lampa.Noty.show('✅ UAFix v' + pluginVersion + ' завантажено!');
+
+        // ГРУБИЙ МЕТОД: Кожну секунду скануємо екран (працює незалежно від подій Лампи)
+        setInterval(function() {
+            // Шукаємо панель з кнопками на екрані
+            var buttonsPanel = $('.full-start__buttons, .info__buttons, .view--buttons').first();
+
+            // Якщо панель є, а нашої кнопки uafix-test-btn ще немає:
+            if (buttonsPanel.length > 0 && $('.uafix-test-btn').length === 0) {
+                
+                // Створюємо кнопку
+                var btn = $('<div class="full-start__button selector uafix-test-btn" style="background: #e50914;"><span>🔴 UAFix Test</span></div>');
+                
+                // Реакція на клік пульта/мишки
+                btn.on('hover:enter click', function () {
+                    Lampa.Noty.show('Ура! Кнопка натиснута!');
+                });
+
+                // Вставляємо кнопку в панель
+                buttonsPanel.append(btn);
             }
-        });
+        }, 1000); // 1000 мілісекунд = 1 секунда
     }
 
-    // НАЙГОЛОВНІШЕ: Запускаємо код ТІЛЬКИ тоді, коли Лампа повністю готова
+    // Запуск
     if (window.appready) {
-        initButton();
+        startPlugin();
     } else {
         Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'ready') {
-                initButton();
-            }
+            if (e.type == 'ready') startPlugin();
         });
     }
-
 })();
